d33fed598e68272a4f1da0f54d0b88990fc39ef9
Kai <kt980715@gmail.com>
2026-01-15 13:52:27 +0100

    Config

---
+++ Configuration/GenProduction/python/gridpacktogen_ggHtoDsW_fragment_cff.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/RunIII/13p6TeV/slc7_amd64_gcc10/powheg/V2/gg_H_quark-mass-effects_mwindow1d0_slc7_amd64_gcc10_CMSSW_12_4_8.tgz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    generateConcurrently = cms.untracked.bool(True),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import * 
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13600.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        pythia8PowhegEmissionVetoSettingsBlock,
+        processParameters = cms.vstring(
+            'POWHEG:nFinal = 1',
+            '25:onMode = off',
+            '25:addChannel = 1  1.00   103   -24   431',
+            '25:m0 = 125.0',
+            '-24:onMode = off',
+            '-24:onIfMatch = -13 14',
+	        '431:onMode = off',
+	        '431:onIfMatch = 333 211',
+	        '333:onMode = off',
+	        '333:onIfMatch = 321 -321',
+        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
+                                    'pythia8PowhegEmissionVetoSettings',
+                                    'processParameters',
+                                    )
+    )
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator)
---
+++ Simulation/LHEGEN/mkconfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# ver=20260114
+# year=2022
+# pnfspath="/store/user/kakang/Analysis/Simulation/${ver}/${year}"
+
+SEED=$(( 10000000 + $1 * 1000 ))
+echo $SEED
+
+mode="SIM"
+
+cfgfile=${mode}_cfg.py
+
+cmsDriver.py "Configuration/GenProduction/python/gridpacktogen_ggHtoDsW_fragment_cff.py" \
+    --eventcontent RAWSIM,LHE \
+    --customise Configuration/DataProcessing/Utils.addMonitoring \
+    --datatier GEN-SIM,LHE \
+    --conditions 130X_mcRun3_2022_realistic_v5 \
+    --beamspot Realistic25ns13p6TeVEarly2022Collision \
+    --step LHE,GEN,SIM \
+    --geometry DB:Extended \
+    --era Run3 \
+    --fileout file:test.root \
+    --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})" \
+    --python_filename "${cfgfile}" \
+    --number 10 \
+    --no_exec \
+    --mc || exit $? ;
